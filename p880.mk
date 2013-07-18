3c64d2fe27e809cf76609fc44395f35ad3a9d32c
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-07-18 18:03:55 +0100

    Apply x3-specific selinux policies
    Change-Id: I3836772fbcaad3062de1665081789a7357bbaa83

--- p880.mk
+++ p880.mk
@@ -116,6 +116,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/init.rmnet-down:system/bin/init.rmnet-down \
     $(LOCAL_PATH)/prebuilt/init.rawip:system/bin/init.rawip
 
+# For SELinux
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/51selinux:system/etc/init.d/51selinux
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_p880
 PRODUCT_DEVICE := p880
