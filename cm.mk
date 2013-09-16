3a9de38913430323680596067ab293e7ea9395ed
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-09-16 20:49:18 +0100

    Update bootanim configuration

--- cm.mk
+++ cm.mk
@@ -1,7 +1,9 @@
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-TARGET_BOOTANIMATION_NAME := vertical-720x1280
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
 
 # Release name
 PRODUCT_RELEASE_NAME := p880
