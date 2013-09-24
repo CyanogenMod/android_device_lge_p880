6b85452c102609c47e5b6a81ea43aa2c17ed05d3
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-09-24 13:27:32 +0100

    radio: Fix clear method name
    Change-Id: I5be43e34e93f706d959dbe37b94349d8a14fad71

--- ril/telephony/java/com/android/internal/telephony/X3RIL.java
+++ ril/telephony/java/com/android/internal/telephony/X3RIL.java
@@ -69,7 +69,7 @@ public class X3RIL extends RIL implements CommandsInterface {
         RILRequest.resetSerial();
 
         // Clear request list
-        clearRequestList(RADIO_NOT_AVAILABLE, false);
+        clearRequestsList(RADIO_NOT_AVAILABLE, false);
         // Thread sleeps are ususally a terrible idea, but we do want the radio
         // stack to back off for a bit
         SystemService.start("ril-daemon");
