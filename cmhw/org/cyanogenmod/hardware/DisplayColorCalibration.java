79b98394b87ce6d05469415c198011daf3f0d620
laufersteppenwolf <laufersteppenwolf@gmail.com>
2014-05-08 19:00:17 +0200

    cmhw: Add missing default Color calibration method
    Change-Id: Ib033f02fe9228b261bf3fab0946271f56ff65828

--- cmhw/org/cyanogenmod/hardware/DisplayColorCalibration.java
+++ cmhw/org/cyanogenmod/hardware/DisplayColorCalibration.java
@@ -28,6 +28,9 @@ public class DisplayColorCalibration {
     public static int getMaxValue()  {
         return 255;
     }
+    public static int getDefValue()  {
+        return getMaxValue();
+    }
     public static int getMinValue()  {
         return 0;
     }
