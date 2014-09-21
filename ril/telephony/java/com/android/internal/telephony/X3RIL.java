82ab88278a87326d84ff16558dc3c1b330dcbbad
demetris <ierokipides.dem@gmail.com>
2014-09-21 16:55:45 +0300

    X3Ril: Fix lge recovery with new ts0710_mux driver. Credit: alfsamsung
    Change-Id: Ic57cee8582241fe8ccf8cd01563ea2aac9775b6b

--- ril/telephony/java/com/android/internal/telephony/X3RIL.java
+++ ril/telephony/java/com/android/internal/telephony/X3RIL.java
@@ -3,8 +3,8 @@ package com.android.internal.telephony;
 import static com.android.internal.telephony.RILConstants.*;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncResult;
-import android.os.Handler;
 import android.os.Message;
 import android.os.Parcel;
 import android.os.SystemService;
@@ -16,6 +16,44 @@ public class X3RIL extends RIL implements CommandsInterface {
 
     public X3RIL(Context context, int networkMode, int cdmaSubscription) {
         super(context, networkMode, cdmaSubscription);
+    }
+
+    private static void x3Sleep(int value) {
+    try {
+            Thread.sleep(value);
+        } catch (InterruptedException ie) {}
+    }
+
+    public void
+    lgeSendCommand(int command) {
+        //RIL_REQUEST_LGE_SEND_COMMAND
+        RILRequest rrLSC = RILRequest.obtain(
+                0x113, null);
+        rrLSC.mParcel.writeInt(1);
+        rrLSC.mParcel.writeInt(command);
+        send(rrLSC);
+        riljLog("X3RIL: LGE COMMAND " + (command) + " sendt");
+    }
+    
+    protected void
+    setSpeechCodec(Object oldret) {
+        if (oldret == null) {
+            riljLog("ERROR: LGE_SELECTED_SPEECH_CODEC is NULL");
+            return;
+        }
+        Intent SelectedSpeechCodecIntent = new Intent("com.lge.ril.SELECTED_SPEECH_CODEC");
+        String SelectedSpeechCodecString = (String)oldret;
+        int SelectedSpeechCodecNumber = Integer.parseInt(SelectedSpeechCodecString);
+
+        if (RILJ_LOGD) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("LGE_SELECTED_SPEECH_CODEC setSpeechCodec = ");
+        sb.append(SelectedSpeechCodecNumber);
+        riljLog(sb.toString());
+        }
+
+        SelectedSpeechCodecIntent.putExtra("SelectedSpeechCodec", SelectedSpeechCodecNumber);
+        mContext.sendBroadcast(SelectedSpeechCodecIntent);
     }
 
     @Override
@@ -65,14 +103,19 @@ public class X3RIL extends RIL implements CommandsInterface {
 
     static final int RIL_UNSOL_LGE_STK_PROACTIVE_SESSION_STATUS = 1041;
     static final int RIL_UNSOL_LGE_NETWORK_REGISTRATION_ERROR = 1047;
+    static final int RIL_UNSOL_LGE_PBREADY = 1049;
     static final int RIL_UNSOL_LGE_BATTERY_LEVEL_UPDATE = 1050;
     static final int RIL_UNSOL_LGE_XCALLSTAT = 1053;
     static final int RIL_UNSOL_LGE_RESTART_RILD = 1055;
+    static final int RIL_UNSOL_LGE_RESPONSE_PS_SIGNALING_STATUS = 1058;
     static final int RIL_UNSOL_LGE_SELECTED_SPEECH_CODEC = 1074;
     static final int RIL_UNSOL_LGE_SIM_STATE_CHANGED_NEW = 1061;
     static final int RIL_UNSOL_LGE_FACTORY_READY = 1080;
 
     private void restartRild() {
+        if (RILJ_LOGD) {
+            riljLog("X3RIL: restartRild started");
+        }
         setRadioState(RadioState.RADIO_UNAVAILABLE);
         SystemService.stop("ril-daemon");
         RILRequest.resetSerial();
@@ -82,9 +125,7 @@ public class X3RIL extends RIL implements CommandsInterface {
         // Thread sleeps are ususally a terrible idea, but we do want the radio
         // stack to back off for a bit
         SystemService.start("ril-daemon");
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException ie) {}
+        x3Sleep(500);
         setRadioState(RadioState.RADIO_ON);
     }
 
@@ -98,12 +139,14 @@ public class X3RIL extends RIL implements CommandsInterface {
         switch(response) {
             case RIL_UNSOL_LGE_STK_PROACTIVE_SESSION_STATUS: ret =  responseVoid(p); break;
             case RIL_UNSOL_LGE_NETWORK_REGISTRATION_ERROR: ret =  responseVoid(p); break;
+            case RIL_UNSOL_LGE_PBREADY: ret =  responseVoid(p); break;
             case RIL_UNSOL_RESPONSE_RADIO_STATE_CHANGED: ret =  responseVoid(p); break;
             case RIL_UNSOL_LGE_BATTERY_LEVEL_UPDATE: ret =  responseVoid(p); break;
             case RIL_UNSOL_LGE_XCALLSTAT: ret =  responseVoid(p); break;
             case RIL_UNSOL_LGE_SELECTED_SPEECH_CODEC: ret =  responseVoid(p); break;
             case RIL_UNSOL_LGE_SIM_STATE_CHANGED_NEW: ret =  responseVoid(p); break;
             case RIL_UNSOL_LGE_RESTART_RILD: ret =  responseVoid(p); break;
+            case RIL_UNSOL_LGE_RESPONSE_PS_SIGNALING_STATUS: ret = responseVoid(p); break;
             case RIL_UNSOL_LGE_FACTORY_READY: ret =  responseVoid(p); break;
             default:
                 // Rewind the Parcel
@@ -121,25 +164,34 @@ public class X3RIL extends RIL implements CommandsInterface {
                 super.processUnsolicited(p);
                 if (RadioState.RADIO_ON == newState) {
                     setNetworkSelectionModeAutomatic(null);
+                } else if (RadioState.RADIO_OFF == newState) {
+                    sentHwBootstrap = false;
                 }
                 return;
             case RIL_UNSOL_LGE_FACTORY_READY:
-                //RIL_REQUEST_LGE_SEND_COMMAND 0
-                RILRequest rrLSC = RILRequest.obtain(
-                        0x113, null);
-                rrLSC.mParcel.writeInt(1);
-                rrLSC.mParcel.writeInt(0);
-                send(rrLSC);
+                if (!sentHwBootstrap) {
+                    lgeSendCommand(1);
+                    sentHwBootstrap = true;
+                } else if (RILJ_LOGD) riljLog("sinking LGE request > " + response);
+                break;
+            case RIL_UNSOL_LGE_PBREADY:
+                  x3Sleep(200);  
+                  lgeSendCommand(0);
                 break;
             case RIL_UNSOL_LGE_RESTART_RILD:
                 restartRild();
                 break;
+            case RIL_UNSOL_LGE_RESPONSE_PS_SIGNALING_STATUS:
+                if (RILJ_LOGD) riljLog("sinking LGE request > " + response);
+                break;
             case RIL_UNSOL_LGE_STK_PROACTIVE_SESSION_STATUS:
             case RIL_UNSOL_LGE_NETWORK_REGISTRATION_ERROR:
             case RIL_UNSOL_LGE_BATTERY_LEVEL_UPDATE:
+                if (RILJ_LOGD) riljLog("sinking LGE request > " + response);
+                break;
             case RIL_UNSOL_LGE_XCALLSTAT:
             case RIL_UNSOL_LGE_SELECTED_SPEECH_CODEC:
-                if (RILJ_LOGD) riljLog("sinking LGE request > " + response);
+                setSpeechCodec(ret);
                 break;
             case RIL_UNSOL_LGE_SIM_STATE_CHANGED_NEW:
                 if (RILJ_LOGD) unsljLog(response);
@@ -150,12 +202,20 @@ public class X3RIL extends RIL implements CommandsInterface {
                 break;
         }
     }
-
+    
+    @Override
+    public void getNeighboringCids(Message response) {
+        AsyncResult.forMessage(response).exception =
+            new CommandException(CommandException.Error.REQUEST_NOT_SUPPORTED);
+        response.sendToTarget();
+        response = null;
+    }
+    
     @Override
     public void getImsRegistrationState(Message result) {
-        if(mRilVersion >= 8)
+        if (mRilVersion >= 8) {
             super.getImsRegistrationState(result);
-        else {
+        } else {
             if (result != null) {
                 CommandException ex = new CommandException(
                     CommandException.Error.REQUEST_NOT_SUPPORTED);
@@ -164,5 +224,4 @@ public class X3RIL extends RIL implements CommandsInterface {
             }
         }
     }
-
 }
