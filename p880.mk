85077c379ad58aa87878de8ae5e5725c12239f51
demetris <ierokipides.dem@gmail.com>
2015-07-22 00:24:44 +0300

    Remove ffmpeg, no longer required and calling it breaks build.
    Change-Id: Id9a9cc4f0887e4efe0454fa9da3a69b150f70b04

--- p880.mk
+++ p880.mk
@@ -27,7 +27,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/nvcamera.conf:system/etc/nvcamera.conf \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
